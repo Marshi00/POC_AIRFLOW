4c4308e42bb757208884c4450acc5bc80aff8637
Marshi00 <112508169+Marshi00@users.noreply.github.com>
2023-09-05 11:38:59 -0400

    w

--- bash_commands.py
+++ bash_commands.py
@@ -9,3 +9,13 @@ docker exec -it poc_airflow-airflow-scheduler-1 /bin/bash
 run a docker file 
 docker-compose up -d
 """
+
+"""
+copy the cfg file from docker to your local
+docker cp poc_airflow-airflow-scheduler-1:/opt/airflow/airflow.cfg .
+"""
+
+"""
+run flower
+docker-compose down && docker-compose --profile flower up -d
+"""
